aa746ca80c05797596fdf88ada699957bdc63d75
Angelo <asantoso1@miumg.edu.gt>
2025-10-24 22:39:04 -0600

    fix: Eliminar opcion cacheComponents incompatible con Next.js 15

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-export const cacheComponents = true;
+//export const cacheComponents = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
